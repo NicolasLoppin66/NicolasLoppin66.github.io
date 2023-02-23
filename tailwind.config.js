57e8316e72317418eb6eae765a27443c765538e7
NicolasLoppin66 <loppin.n66@gmail.com>
2023-02-23 19:00:32 +0100

    Update : Style du site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  // Ajout du plugin DaisyUI
-  plugins: [require('daisyui')],
-  // Configuration de DaisyUI (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+  plugins: [],
 }
